bc13286e8ffa5a678c6cff5918b8885ca8f8a014
hammaadworks <hammaadworks@gmail.com>
2026-09-22 07:23:18 +0530

    coins diff

--- whatchadoin/vite.config.js
+++ whatchadoin/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
+    plugins: [react()], resolve: {
         tsconfigPaths: true
-    },
-    build: {
-        chunkSizeWarningLimit: 1000
-    },
-    server: {
+    }, build: {
+        chunkSizeWarningLimit: 1000, rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('recharts')) return 'recharts';
+                        if (id.includes('lucide-react')) return 'lucide';
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    }, server: {
         allowedHosts: ["alhamdulillahs-macbook-pro.local"]
     }
 });
--- whatchadoin/vite.config.ts
+++ whatchadoin/vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    tsconfigPaths: true
-  },
-  build: {
-    chunkSizeWarningLimit: 1000
-  },
-  server: {
-    allowedHosts: ["alhamdulillahs-macbook-pro.local"]
-  }
+    plugins: [react()], resolve: {
+        tsconfigPaths: true
+    }, build: {
+        chunkSizeWarningLimit: 1000, rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('recharts')) return 'recharts';
+                        if (id.includes('lucide-react')) return 'lucide';
+                        return 'vendor';
+                    }
+                }
+            }
+        }
+    }, server: {
+        allowedHosts: ["alhamdulillahs-macbook-pro.local"]
+    }
 })
